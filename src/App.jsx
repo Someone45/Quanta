7e10c8783b5cba4863eeca3eb31714b8650c3617
Adriel Fung <AdrielF31@gmail.com>
2024-02-10 16:12:48 -0500

    Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage.jsx';
+import HomePage from './pages/home/HomePage.jsx';
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
+                <Route path="/home" element={<HomePage />} />
             </Routes>
         </Router>
     );
