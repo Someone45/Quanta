d79b249391bf5505eca1e8d3145cb0a51a0ea2d5
TurtleArmyMc <44322335+TurtleArmyMc@users.noreply.github.com>
2024-02-11 06:34:17 -0500

    Hide record button until ready

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -551,7 +551,7 @@ export default function NewPage() {
                                 sx={{ width: 300 }}
                                 renderInput={(params) => <TextField {...params} label="Friend ID" />}
                                 />
-                            {activeTab === 0 && channel &&  (
+                            {activeTab === 0 && channel.length &&  (
                                 <Box>
                                     <form onSubmit={postFiles}>
                                         <input
@@ -589,7 +589,7 @@ export default function NewPage() {
                                     </form>
                                 </Box>
                             )}
-                            {activeTab === 1 && channel.length > 0 && (
+                            {activeTab === 1 && channel.length && speakerId.length && (
                                 <Box>
                                     <Button style={{"borderRadius": "100%"}} onClick={handleRecordButton}>
                                         {(!recorder) && (
